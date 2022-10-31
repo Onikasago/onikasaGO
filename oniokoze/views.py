dc940a93daa37038a02da0c274e312e615c2e3d5
keigonarita <ykh2135242@stu.o-hara.ac.jp>
2022-10-31 15:45:26 +0900

    オニカサGO

--- oniokoze/views.py
+++ oniokoze/views.py
@@ -1,10 +1,5 @@
 import logging
-from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
-from.models import Catch
-from django.shortcuts import get_object_or_404
 
 logger = logging.getLogger(__name__)
 
