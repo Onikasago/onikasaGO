7d5ca0966ed53c5338f2b8b0867ea489578c1fd9
keigonarita <ykh2135242@stu.o-hara.ac.jp>
2022-11-14 11:42:03 +0900

    11/14 成田

--- oniokoze/urls.py
+++ oniokoze/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'oniokoze'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('catch-list', views.CatchListView.as_view(), name="catch_list"),
-    path('catch-create', views.CatchCreateView.as_view(), name="catch_create"),
+    path('catch-list/', views.CatchListView.as_view(), name="catch_list"),
+    path('catch-create/', views.CatchCreateView.as_view(), name="catch_create"),
+    path('spot-list/', views.SpotListView.as_view(), name="spot_list"),
 
 
 ]
