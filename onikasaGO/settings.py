819aa5af218c37ea09ae070faa1654667d17a401
yuutaroudesuga5615 <ykh2135106@stu.o-hara.ac.jp>
2022-11-21 15:06:53 +0900

    11/21 鈴木悠太 モデル修正

--- onikasaGO/settings.py
+++ onikasaGO/settings.py
@@ -96,8 +96,8 @@ LOGGING = {
             'handlers': ['console'],
             'level': 'INFO',
         },
-        # diaryアプリケーションが利用するロガー
-        'list': {
+        # oniokozeアプリケーションが利用するロガー
+        'oniokoze': {
             'handlers': ['console'],
             'level': 'DEBUG',
         },
@@ -174,46 +174,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-
-    #ロガーの設定
-    'loggers': {
-        #Djangoが利用するロガー
-        'django': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        },
-        #onikasaGOアプリケーションが利用するロガー
-        'onikasaGO': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
-
-    #ハンドラの設定
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'dev'
-        },
-    },
-
-    #フォーマッタの設定
-    'formatters': {
-        'dev': {
-            'format': '\t'.join([
-                '%(asctime)s',
-                '[%(levelname)s]',
-                '%(pathname)s(Line:%(lineno)d)',
-                '%(message)s'
-            ])
-        },
-    }
-}
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
