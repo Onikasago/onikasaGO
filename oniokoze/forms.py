a9866232c9caf1eef57e6c660e7b40361cc46ae3
21-35006 <ykh2135006@stu.o-hara.ac.jp>
2022-11-28 09:41:44 +0900

    11/28 渡辺

--- oniokoze/forms.py
+++ oniokoze/forms.py
@@ -1,36 +1,50 @@
 from django import forms
 import os
-from .models import Catch,Fishname
+import logging
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Catch, Fishname
+from django.forms.models import inlineformset_factory
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
+
+
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        catch = get_object_or_404(Fishname, pk=self.kwargs['pk'])
+        return self.request.user == list.user
 
 
 class CatchCreateForm(forms.ModelForm):
-
-
-    class Meta():
-
+    class Meta:
         model = Catch
         fields = (
-            'capital', 'city', 'address', 'location', 'free'
+            'nametitle', 'photo1', 'capital', 'city', 'address', 'place', 'location', 'free'
+
         )
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             for field in self.fields.values():
                 field.widget.attrs['class'] = 'form-control'
-
 
 
 class FishnameCreateForm(forms.ModelForm):
-
-
-    class Meta():
-
+    class Meta:
         model = Fishname
         fields = (
-            'name', 'size'
+            '__all__'
         )
+
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             for field in self.fields.values():
                 field.widget.attrs['class'] = 'form-control'
 
+        def form_valid(self, form):
+            catch = form.save(commit=False)
+            catch.user = self.request.user
+            catch.save()
+            return super().form_vaild(form)
--- oniokoze/views.py
+++ oniokoze/views.py
@@ -1,22 +1,23 @@
 import logging
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import CatchCreateForm,FishnameCreateForm
-from .models import Catch,Fishname
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+import re
+from .forms import CatchCreateForm, FishnameCreateForm
+from .models import Catch, Fishname
 from django.shortcuts import render
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 
-
 logger = logging.getLogger(__name__)
 
+
 class IndexView(generic.TemplateView):
-    template_name = "index/.html"
+    template_name = "index.html"
 
 
 class CatchListView(LoginRequiredMixin, generic.ListView):
-    model = Catch
+    model = Fishname.objects.select_related('catch').all()
     template_name = 'catch_list.html'
 
     def get_queryset(self):
@@ -24,23 +25,72 @@ class CatchListView(LoginRequiredMixin, generic.ListView):
         return catches
 
 
+class OnlyYouMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        dairy = get_object_or_404(Diary, pk=self.kwargs['pk'])
+        return self.request.user == list.user
+
+
+class CatchDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Fishname
+    template_name = 'catch_detail.html'
+
+
 class CatchCreateView(generic.CreateView):
-    model = Catch
+    model = Fishname
     form_class = CatchCreateForm
     template_name = 'catch_create.html'
-    success_url = reverse_lazy('oniokoze:catch-list')
+    success_url = reverse_lazy('oniokoze:catch_list')
+
+    def form_valid(self, form):
+        catch = form.save(commit=False)
+        catch.user = self.request.user
+        catch.save()
+
+
+
+def get_queryset():
+    catches = Fishname.objects.select_related('catch').all()
+    return catches
+
 
 class FishnameCreateView(generic.CreateView):
-    model = Fishname
     form_class = FishnameCreateForm
     template_name = 'fishname_create.html'
-    success_url = reverse_lazy('oniokoze:catch-create')
+    success_url = reverse_lazy('oniokoze:catch_create')
 
-class SpotListView(LoginRequiredMixin,generic.TemplateView):
+    def post(self, request, *args, **kwrgs):
+          # 空の配列を作ります
+        titleList= []
+        bodyList= []
+          # request.POST.items()でPOSTで送られてきた全てを取得。
+        for i in request.POST.items():
+            if re.match(r'title_*', i[0]):
+                title.append(i[1])
+            if re.match(r'body_*', i[0]):
+                body.append(i[1])
+
+        for i in range(len(title)):
+            corporationinformation = CorporationInformation(
+            title= titleList[i],
+                body = bodyList[i],
+            )
+            corporationinformation.save()
+        return redirect(to='/home')
+
+class SpotListView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'spot_list.html'
-class ResipeListView(LoginRequiredMixin,generic.TemplateView):
+
+
+class ResipeListView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'resipe_list.html'
+
+
 class TriviaView(generic.TemplateView):
     template_name = 'trivia.html'
+
+
 class MypageView(generic.TemplateView):
     template_name = 'mypage.html'
