4c897b33c9e8f06c34fe7d916ec23c8629b6a8c0
yuutaroudesuga5615 <ykh2135106@stu.o-hara.ac.jp>
2023-01-31 10:24:13 +0900

    1/30 ファイル ボビーオロゴン

--- oniokoze/urls.py
+++ oniokoze/urls.py
@@ -3,7 +3,6 @@ from . import views
 from .views import *
 # from django.contrib.staticfiles.urls import static
 
-from django.contrib.staticfiles.urls import static
 app_name = 'oniokoze'
 
 urlpatterns = [
@@ -15,24 +14,28 @@ urlpatterns = [
     path('catch-delete/<int:pk>/', views.CatchDeleteView.as_view(), name="catch_delete"),
     path('fishname-create', views.FishnameCreateView.as_view(), name="fishname_create"),
     path('fishname-update/<int:pk>/', views.FishnameUpdateView.as_view(), name="fishname_update"),
+
     path('spot-list', views.SpotListView.as_view(), name="spot_list"),
     path('spot-create', views.SpotCreateView.as_view(), name="spot_create"),
     path('spot-detail/<int:pk>/', views.SpotDetailView.as_view(), name="spot_detail"),
     path('spot-update/<int:pk>', views.SpotUpdateView.as_view(), name="spot_update"),
     path('spot-delete/<int:pk>/', views.SpotDeleteView.as_view(), name="spot_delete"),
-    path('fish-create', views.FishCreateView.as_view(), name="fish_create"),
     path('recipe-list', views.RecipeListView.as_view(), name="recipe_list"),
     path('recipe-create', views.RecipeCreateView.as_view(), name="recipe_create"),
     path('recipe-detail/<int:pk>/', views.RecipeDetailView.as_view(), name="recipe_detail"),
     path('recipe-update/<int:pk>/', views.RecipeUpdateView.as_view(), name="recipe_update"),
     path('recipe-delete/<int:pk>/', views.RecipeDeleteView.as_view(), name="recipe_delete"),
-    path('order-create', views.OrderCreateView.as_view(), name="order_create"),
+    path('order-create',views.OrderCreateView.as_view(),name="order_create"),
     path('like_for_spot/', views.like_for_spot, name='like_for_spot'),
     path('like_for_catch/', views.like_for_catch, name='like_for_catch'),
     path('like_for_recipe/', views.like_for_recipe, name='like_for_recipe'),
     path('trivia/', views.TriviaView.as_view(), name="trivia"),
     path('mypage/', views.MypageView.as_view(), name="mypage"),
-    path('process-form', processForm, name='process-form'),
-    path('get-prefecture', getPrefecture, name='get-prefecture'),
-
+    path('get-prefecture', views.getPrefecture, name='get-prefecture'),
+    path('tool/', views.ToolView.as_view(), name="tool"),
+    path('manners/', views.MannersView.as_view(), name="manners"),
+    path('fishing_method/', views.Fishing_methodView.as_view(), name="fishing_method"),
+    path('bait/', views.BaitView.as_view(), name="bait"),
+    path('dangerous_creature/', views.Dangerous_creatureView.as_view(), name="dangerous_creature"),
+    path('place/', views.PlaceView.as_view(), name="place"),
 ]
