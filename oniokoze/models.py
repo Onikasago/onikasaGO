fafadef69c3f9e7726715e08894b2ec65134ce62
21-35006 <ykh2135006@stu.o-hara.ac.jp>
2023-02-10 10:43:17 +0900

    "2/10渡辺  "

--- oniokoze/models.py
+++ oniokoze/models.py
@@ -2,83 +2,6 @@ from accounts.models import CustomUser
 from django.utils import timezone
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-class History(models.Model):
-    CAPITALS = (
-        ('北海道', '北海道'),
-        ('青森県', '青森県'),
-        ('岩手県', '岩手県'),
-        ('宮城県', '宮城県'),
-        ('秋田県', '秋田県'),
-        ('山形県', '山形県'),
-        ('福島県', '福島県'),
-        ('茨城県', '茨城県'),
-        ('栃木県', '栃木県'),
-        ('群馬県', '群馬県'),
-        ('埼玉県', '埼玉県'),
-        ('千葉県', '千葉県'),
-        ('東京都', '東京都'),
-        ('神奈川県', '神奈川県'),
-        ('新潟県', '新潟県'),
-        ('富山県', '富山県'),
-        ('石川県', '石川県'),
-        ('福井県', '福井県'),
-        ('山梨県', '山梨県'),
-        ('長野県', '長野県'),
-        ('岐阜県', '岐阜県'),
-        ('静岡県', '静岡県'),
-        ('愛知県', '愛知県'),
-        ('三重県', '三重県'),
-        ('滋賀県', '滋賀県'),
-        ('京都府', '京都府'),
-        ('大阪府', '大阪府'),
-        ('兵庫県', '兵庫県'),
-        ('奈良県', '奈良県'),
-        ('和歌山県', '和歌山県'),
-        ('鳥取県', '鳥取県'),
-        ('島根県', '島根県'),
-        ('岡山県', '岡山県'),
-        ('広島県', '広島県'),
-        ('山口県', '山口県'),
-        ('徳島県', '徳島県'),
-        ('香川県', '香川県'),
-        ('愛媛県', '愛媛県'),
-        ('高知県', '高知県'),
-        ('福岡県', '福岡県'),
-        ('佐賀県', '佐賀県'),
-        ('長崎県', '長崎県'),
-        ('熊本県', '熊本県'),
-        ('大分県', '大分県'),
-        ('宮崎県', '宮崎県'),
-        ('鹿児島県', '鹿児島県'),
-        ('沖縄県', '沖縄県')
-    )
-
-    LOCATIONS = (
-        ('海', '海'),
-        ('川', '川'),
-        ('管理釣り場', '管理釣り場'),
-        ('船', '船'),
-        ('湖', '湖'),
-        ('その他', 'その他')
-    )
-
-    user = models.ForeignKey(CustomUser, verbose_name='ユーザID', on_delete=models.PROTECT)
-    capital = models.TextField(choices=CAPITALS, verbose_name='都道府県')
-    city = models.TextField(verbose_name='市区町村', blank=True)
-    address = models.TextField(verbose_name='番地以降', blank=True)
-    place = models.TextField(verbose_name='釣り場', blank=True)
-    free = models.TextField(verbose_name='自由記入欄', blank=True, null=True)
-    spotURL = models.URLField(verbose_name='URL記入欄', blank=True, null=True)
-    location = models.CharField(choices=LOCATIONS, blank=True, null=True, max_length=5)
-    beginner = models.BooleanField(verbose_name='初心者おすすめチェック', default=False, help_text='初心者おすすめ', blank=True,null=True)
-
-    class Meta:
-        verbose_name_plural = 'Spotlike'
-
-    def __str__(self):
-        return self.CAPITALS
-
-
 
 class Catch(models.Model):
     CAPITALS = (
@@ -142,7 +65,7 @@ class Catch(models.Model):
 
 
     nametitle = models.TextField(verbose_name='釣果タイトル')
-    photo1 = models.ImageField(verbose_name='写真1', blank=True, null=True)
+    photo1 = models.ImageField(verbose_name='写真1')
     photo2 = models.ImageField(verbose_name='写真2', blank=True, null=True)
     photo3 = models.ImageField(verbose_name='写真3', blank=True, null=True)
     photo4 = models.ImageField(verbose_name='写真4', blank=True, null=True)
@@ -234,7 +157,6 @@ class Spot(models.Model):
     address = models.TextField(verbose_name='番地以降', blank=True)
     place = models.TextField(verbose_name='釣り場', blank=True)
     free = models.TextField(verbose_name='自由記入欄', blank=True, null=True)
-    URLcheck = models.CharField(verbose_name='URLチェック', blank=True, null=True, max_length=1)
     spotURL = models.URLField(verbose_name='URL記入欄', blank=True, null=True)
     location = models.CharField(choices=LOCATIONS, verbose_name='ロケーション',blank=True, null=True, max_length=5)
     beginner = models.BooleanField(verbose_name='初心者おすすめチェック',default=False, help_text='初心者おすすめ',blank=True,null=True)
